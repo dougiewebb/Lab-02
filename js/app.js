36d146027db3a479fe243c800f63d7ba01bc7a7f
Doug <dougie.webb77@gmail.com>
2023-04-15 17:09:50 -0500

    added

---
+++ js/app.js
@@ -0,0 +1,16 @@
+alert ("Hello World")
+
+ // What is your Name?
+
+ let answerOne = prompt( "Do I have dogs?");
+ let answerTwo = prompt( "Do I like to Cook?");
+ let answerThree = prompt( "Do I have 2 kids?");
+ let answerFour = prompt( "Am I orginally from Alabama?");
+ let answerFive = prompt( "Do I like sports");
+
+ if(answerOne === 'y' || answerOne ==='yes'){ 
+    console.log ("You are correct!");
+ } else {
+     // if 'n' then you are incorrect
+     console.log("You are inccorrect!");
+ }    
