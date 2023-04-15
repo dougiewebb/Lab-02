318eb7e69e2691ae7575bc12fa7d0942a8a0a909
Doug <dougie.webb77@gmail.com>
2023-04-15 17:47:37 -0500

    Updated with about me section

--- js/app.js
+++ js/app.js
@@ -1,16 +1,52 @@
-alert ("Hello World")
+'use strict'
+//TODO: ask the user's name
+let name = prompt("Thanks for stopping by!  What is your name?")
 
- // What is your Name?
-
+//TODO: Ask y/n questions
  let answerOne = prompt( "Do I have dogs?");
  let answerTwo = prompt( "Do I like to Cook?");
  let answerThree = prompt( "Do I have 2 kids?");
  let answerFour = prompt( "Am I orginally from Alabama?");
  let answerFive = prompt( "Do I like sports");
 
+ //Use if / else
+ //if 'y' then you are correct
+
  if(answerOne === 'y' || answerOne ==='yes'){ 
     console.log ("You are correct!");
  } else {
      // if 'n' then you are incorrect
      console.log("You are inccorrect!");
- }    
+ }  
+ 
+ if(answerTwo === 'y' || answerTwo === 'yes'){
+    console.log("You are correct!");
+ } else {
+    // if 'n' then you are incorrect
+    console.log("You are inccorrect!")
+ }
+
+ if(answerThree === 'y' || answerThree ==='yes'){ 
+    console.log ("You are correct!");
+ } else {
+     // if 'n' then you are incorrect
+     console.log("You are inccorrect!");
+ }  
+ 
+ if(answerFour === 'y' || answerFor ==='yes'){ 
+    console.log ("You are correct!");
+ } else {
+     // if 'n' then you are incorrect
+     console.log("You are inccorrect!");
+ } 
+
+ if(answerFive === 'y' || answerFive ==='yes'){ 
+    console.log ("You are correct!");
+ } else {
+     // if 'n' then you are incorrect
+     console.log("You are inccorrect!");
+ } 
+ 
+ //TODO: Give a response with an allert
+
+ // Notify user if they are correct
