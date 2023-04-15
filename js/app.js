45d029214cb1260c8c1fdf190ada34ae87bb781f
Doug <dougie.webb77@gmail.com>
2023-04-15 18:27:03 -0500

    css update

--- js/app.js
+++ js/app.js
@@ -1,47 +1,46 @@
 'use strict'
-//TODO: ask the user's name
-let name = prompt("Thanks for stopping by!  What is your name?")
 
-//TODO: Ask y/n questions
- let answerOne = prompt( "Do I have dogs?");
- let answerTwo = prompt( "Do I like to Cook?");
- let answerThree = prompt( "Do I have 2 kids?");
- let answerFour = prompt( "Am I orginally from Alabama?");
- let answerFive = prompt( "Do I like sports");
+let answerOne = prompt("Do I have dogs?").toLowerCase()
+let answerTwo = prompt("Do I live in TN?").toLowerCase()
+let answerThree = prompt ("I am 50 years old?").toLowerCase()
+let answerFour = prompt ("I have 4 kids?").toLowerCase()
 
 
- if(answerOne === 'y' || answerOne ==='yes'){ 
-    console.log("You are Correct!");
- } else {
-     
-     console.log("You are inccorrect!");
- }  
- 
- if(answerTwo === 'y' || answerTwo === 'yes'){
-    console.log("You are correct!");
- } else {
-    
-    console.log("You are inccorrect!")
- }
-
- if(answerThree === 'y' || answerThree ==='yes'){ 
-    console.log("You are correct!");
- } else {
-     
-     console.log("You are inccorrect!");
- }  
- 
- if(answerFour === 'y' || answerFor ==='yes'){ 
-    console.log ("You are correct!");
- } else {
-     // if 'n' then you are incorrect
-     console.log("You are inccorrect!");
- } 
-
- if(answerFive === 'y' || answerFive ==='yes'){ 
-    console.log ("You are correct!");
- } else {
-     // if 'n' then you are incorrect
-     console.log("You are inccorrect!");
- } 
+// if 'y' then you are correct
+if(answerOne === 'y' ||
+ answerOne ==='yes'){
+    alert("You are correct!")
+    console.log("You are correct.");
+} else {
+    // if 'n' then you are incorrect
+    alert("You are incorrect!")
+    console.log("You are incorrect!");
+}
+if(answerTwo === 'y' ||
+ answerTwo ==='yes'){
+    alert("You are correct!")
+    console.log("You are correct.");
+} else {
+    // if 'n' then you are incorrect
+    alert("You are incorrect!")
+    console.log("You are incorrect!");
+}
+if(answerThree === 'n' ||
+ answerThree ==='no'){
+    alert("You are correct!")
+    console.log("You are correct.");
+} else {
+    // if 'y' then you are incorrect
+    alert("You are incorrect!")
+    console.log("You are incorrect!");
+}
+if(answerFour === 'y' ||
+ answerFour ==='yes'){
+    alert("You are correct!")
+    console.log("You are correct.");
+} else {
+    // if 'n' then you are incorrect
+    alert("You are incorrect!")
+    console.log("You are incorrect!");
+}
  
