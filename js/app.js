2863751e3efb228c66d93b395196bd6d0c4f6384
Doug <dougie.webb77@gmail.com>
2023-04-16 11:32:34 -0500

    Updated JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 'use strict'
+//TODO: ask the user's name
+let name = prompt ("Welcome to my Page! What is your name?");
+
 
 let answerOne = prompt("Do I have dogs?").toLowerCase()
 let answerTwo = prompt("Do I live in TN?").toLowerCase()
 let answerThree = prompt ("I am 50 years old?").toLowerCase()
 let answerFour = prompt ("I have 4 kids?").toLowerCase()
+let answerFive = prompt ("Do I like Basketball?").toLowerCase()
 
 
 // if 'y' then you are correct
@@ -44,3 +48,12 @@ if(answerFour === 'y' ||
     console.log("You are incorrect!");
 }
  
+if(answerFive === 'y' ||
+ answerFive ==='yes'){
+    alert("You are correct!")
+    console.log("You are correct.");
+} else {
+    // if 'n' then you are incorrect
+    alert("You are incorrect!")
+    console.log("You are incorrect!");
+}
