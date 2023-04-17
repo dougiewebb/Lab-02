277e7f8bbe087693fae12a701cda0c87502f2d16
Doug <dougie.webb77@gmail.com>
2023-04-17 06:15:22 -0500

    new updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-'use strict'
-//TODO: ask the user's name
-let name = prompt ("Welcome to my Page! What is your name?");
+'use strict';
+
+let name;
+Response = prompt ("What is your name?");
+console.log ("Nice to meet you: " + Response);
 
 
 let answerOne = prompt("Do I have dogs?").toLowerCase()
@@ -8,52 +10,67 @@ let answerTwo = prompt("Do I live in TN?").toLowerCase()
 let answerThree = prompt ("I am 50 years old?").toLowerCase()
 let answerFour = prompt ("I have 4 kids?").toLowerCase()
 let answerFive = prompt ("Do I like Basketball?").toLowerCase()
+let answerSix = prompt ("I attend David Lipscomp University?").toLocaleLowerCase()
 
 
-// if 'y' then you are correct
-if(answerOne === 'y' ||
- answerOne ==='yes'){
-    alert("You are correct!")
-    console.log("You are correct.");
+
+if(answerOne === 'y' || answerOne ==='yes'){
+    //console.log("You are correct.");
+    alert("You are correct!")   
 } else {
     // if 'n' then you are incorrect
+    //console.log("You are incorrect!");
     alert("You are incorrect!")
-    console.log("You are incorrect!");
+    
 }
-if(answerTwo === 'y' ||
- answerTwo ==='yes'){
-    alert("You are correct!")
-    console.log("You are correct.");
+
+if(answerTwo === 'y' || answerTwo ==='yes'){
+    //console.log("You are correct.");
+    alert("You are correct!") 
 } else {
     // if 'n' then you are incorrect
+    //console.log("You are incorrect!");
     alert("You are incorrect!")
-    console.log("You are incorrect!");
+    
 }
-if(answerThree === 'n' ||
- answerThree ==='no'){
-    alert("You are correct!")
-    console.log("You are correct.");
+
+if(answerThree === 'n' || answerThree ==='no'){
+    //console.log("You are correct.");
+    alert("You are correct!")  
 } else {
     // if 'y' then you are incorrect
+    //console.log("You are incorrect!");
     alert("You are incorrect!")
-    console.log("You are incorrect!");
+    
 }
-if(answerFour === 'y' ||
- answerFour ==='yes'){
+
+if(answerFour === 'y' || answerFour ==='yes'){
+    //console.log("You are correct.");
     alert("You are correct!")
-    console.log("You are correct.");
 } else {
     // if 'n' then you are incorrect
+    //console.log("You are incorrect!");
     alert("You are incorrect!")
-    console.log("You are incorrect!");
+    
 }
  
-if(answerFive === 'y' ||
- answerFive ==='yes'){
+if(answerFive === 'y' || answerFive ==='yes'){
+    //console.log("You are correct.");
     alert("You are correct!")
-    console.log("You are correct.");
 } else {
     // if 'n' then you are incorrect
+    //console.log("You are incorrect!");
     alert("You are incorrect!")
-    console.log("You are incorrect!");
+   
 }
+
+if(answerSix === 'n' || answerSix ==='no'){
+    //console.log("You are correct.");
+    alert("You are correct!")
+} else {
+    // if 'y' then you are incorrect
+    //console.log("You are incorrect!");
+    alert("You are incorrect!")
+
+}    
+
